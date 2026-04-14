9b40e4d63bcc36d8eb499e7acd67e3557d0bd4fc
Luna <lunaperera152@gmail.com>
2026-04-14 13:42:03 +0200

    Comentarios añadidos

--- CCuenta.java
+++ CCuenta.java
@@ -1,25 +1,31 @@
-
 package cuentas;
- /*hola */
+
 public class Main {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
+        CCuenta cuenta1;
+        double saldoActual;
 
-    CCuenta cuenta1 = new CCuenta("Alejandro", "AK105286468", 1500, 1.5);
-    operativa_cuenta(cuenta1);
-}
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
 
+        try {
+            cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
 
-
- public static void operativa_cuenta(CCuenta cuenta1, float cantidad) throws Exception {
-
-        cuenta1.ingresar(cantidad);
-        cuenta1.retirar(cantidad);
-        cuenta1.retirar(cantidad);
-
-        System.out.println("Saldo final: " + cuenta1.estado());
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+        } 
+        catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
     }
 }
+
 
 
 
@@ -38,52 +44,91 @@ public class CCuenta {
     public CCuenta(String nom, String cuenta1, double sal, double tipo)
     {
         nombre=nom;
-        cuenta=cuenta1;
+        cuenta1=cuenta1;
         saldo=sal;
         tipoInterés=tipo
     }
 
-
+   /**
+     * Obtiene el nombre del titular.
+     * @return nombre del titular
+     */
 
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Establece el nombre del titular.
+     * @param nombre nuevo nombre
+     */
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+   /**
+     * Obtiene el número de cuenta.
+     * @return número de cuenta
+     */
 
     public String getCuenta() {
-        return cuenta;
+        return cuenta1;
     }
 
-    public void setCuenta(String cuenta) {
-        this.cuenta = cuenta;
+/**
+     *
+     * Establece el número de cuenta.
+     * @param cuenta1 nuevo número de cuenta
+     */
+
+    public void setCuenta(String cuenta1) {
+        this.cuenta1 = cuenta1;
     }
 
+ /**
+     * Obtiene el saldo actual.
+     * @return saldo disponible
+     */
 
     public double getSaldo() {
         return saldo;
     }
 
+  /**
+     * Obtiene el tipo de interés.
+     * @return tipo de interés
+     */
 
     public double getTipoInteres() {
         return tipoInteres;
     }
 
-
+/**
+     * Establece el tipo de interés.
+     * @param tipoInteres nuevo tipo de interés
+     */
 
     public void setTipoInteres(double tipoInteres) {
         this.tipoInteres = tipoInteres;
     }
 
-
+/**
+     * Devuelve el estado actual de la cuenta (saldo).
+     * @return saldo actual
+     */
 
     public double estado()
     {
         return saldo;
     }
+
+/**
+     * Ingresa una cantidad en la cuenta.
+     * 
+     * @param cantidad dinero a ingresar
+     * @throws Exception si la cantidad es negativa
+     */
 
     public void ingresar(double cantidad) throws Exception
     {
