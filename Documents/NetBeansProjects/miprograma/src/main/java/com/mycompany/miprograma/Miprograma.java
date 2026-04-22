c1cbe6b19c50e1b378bd2f50fd1706ae5194be52
Luna <lunaperera152@gmail.com>
2026-04-22 09:36:18 +0200

    primer cooomit

---
+++ Documents/NetBeansProjects/miprograma/src/main/java/com/mycompany/miprograma/Miprograma.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.miprograma;
+
+/**
+ *
+ * @author Luna
+ */
+public class Miprograma {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
